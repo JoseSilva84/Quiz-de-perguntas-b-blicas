a309cfad5dfdccc98a29eac92b865b4dfe009e7a
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-05-08 16:10:46 -0300

    editando js1

---
+++ index.js
@@ -0,0 +1,19 @@
+pergunta1 = {
+    "A": "Adão", 
+    "B": "Caim",
+    "C": "Abraão",
+    "D": "Moisés",
+};
+
+resposta1 = {"B": "Caim"}
+
+pergunta = document.getElementById("pergunta");
+pergunta.innerHTML = `
+    <h1>Quem matou Abel?</h1>
+    <ul>
+        <li></li><br></li>
+        <li class="liA">A: ${pergunta1.A}</li>
+        <li class="liB">B: ${pergunta1.B}</li>
+        <li class="liC">C: ${pergunta1.C}</li>
+        <li class="liD">D: ${pergunta1.D}</li>
+    </ul>`;
