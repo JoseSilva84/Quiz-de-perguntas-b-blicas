ff0a2fe7699ed8783bd2a62e3ee65caadd354df2
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-05-09 14:57:20 -0300

    editando js2

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ pergunta.innerHTML = `
     <h1>Quem matou Abel?</h1>
     <ul>
         <li></li><br></li>
-        <li class="liA">A: ${pergunta1.A}</li>
-        <li class="liB">B: ${pergunta1.B}</li>
-        <li class="liC">C: ${pergunta1.C}</li>
-        <li class="liD">D: ${pergunta1.D}</li>
+        <li class="liA">A - ${pergunta1.A}</li>
+        <li class="liB">B - ${pergunta1.B}</li>
+        <li class="liC">C - ${pergunta1.C}</li>
+        <li class="liD">D - ${pergunta1.D}</li>
     </ul>`;
